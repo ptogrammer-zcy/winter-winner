d450da9aa28e2a0246d050c577ff19f01f2021d6
低微鄙陋 <3108226154@qq.com>
2023-01-01 19:32:06 +0800

    123

---
+++ src/codetest.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class codetest {
+    public static void main(String[] args) {
+        int[] a= {1,2,3,4,5,6};
+//        System.out.println(Arrays.toString(a));
+        /*数组拷贝
+        * 将一个数组的所有值拷贝到一个新的数组中*/
+//        int[] temp=Arrays.copyOf(a,a.length);
+        if(args.length==0||args[0].equals("-h")){
+            System.out.println("hello,");
+        }
+
+    }
+
+}
